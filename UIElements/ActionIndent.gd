5d33de1f5693fe73736a9eee1de563f7fde7ff7e
Bosco <bosco.publicvoid@gmail.com>
2023-10-23 14:00:29 +0200

    actions assigned to character drag and drop

--- UIElements/ActionIndent.gd
+++ UIElements/ActionIndent.gd
@@ -23,14 +23,19 @@ func _can_drop_data(at_position, data):
 
 func _drop_data(at_position, data):
 	var slot = _get_index()
+	print("before:")
+	print(slot)
+	print(data["action"].operations)
+	print(Game.characters[0].actions)
 	Game.characters[0].actions[slot] = data["action"]
+	print("after:")
 	print(data["action"].operations[0])
 	print(Game.characters[0].actions)
 	
 	
 	var fullCycleResult = FullCycleResultEvaluator.evaluate(Game)
+	print("outcome")
 	print(fullCycleResult.resources)
-	print(fullCycleResult.resources.rocks)
 
 func _get_index():
 	return get_parent().get_children().find(self)
