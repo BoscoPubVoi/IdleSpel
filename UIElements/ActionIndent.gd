1fe2b1f84a2c82d0dc5494eae0d7e788a93fd34e
Bosco <bosco.publicvoid@gmail.com>
2023-10-23 22:27:17 +0200

    merge + fix spacing of characters

--- UIElements/ActionIndent.gd
+++ UIElements/ActionIndent.gd
@@ -10,8 +10,11 @@ var current_data = null
 func _ready():
 	pass
 
+
+	
 func _get_drag_data(at_position):
 	if current_data != null:
+		print(current_data)
 		var data = current_data
 		data["origin_node"] = self
 		var drag_texture = TextureRect.new()
@@ -24,6 +27,7 @@ func _get_drag_data(at_position):
 		drag_texture.position = -0.5 * (drag_texture.size)
 		data["texture"] = drag_texture
 		set_drag_preview(control)
+		
 		return data
 
 func _can_drop_data(at_position, data):
@@ -35,11 +39,9 @@ func _can_drop_data(at_position, data):
 	return true
 
 func _drop_data(at_position, data):
-#	data["target_slot"] = null
 	var origin_slot = data["origin_node"]
 	var origin_origin = data["original_original"]
 	if data["target_slot"] == null:
-		print("asdausadua")
 		put_in_slot(data)
 		
 		#empty the old one
@@ -55,11 +57,11 @@ func _drop_data(at_position, data):
 		if origin_slot is ActionIcon:
 			origin_slot.make_used(true)
 			put_in_slot(data)
-			#enable the one that was replaced in the actionicons TODO
 			get_tree().get_first_node_in_group("ActionsPanel").reset_action(data["target_slot"]["original_original"])
 		else:
 			#not empty and your trying to replace it
 			var tempdata = data
+#			print(data["target_slot"])
 			origin_slot.put_in_slot(data["target_slot"])
 			put_in_slot(tempdata)
 
@@ -69,24 +71,24 @@ func put_in_slot(data):
 	var par = get_parent()
 	while par != null && !par.is_in_group("ActorPanel"):
 		par = par.get_parent()
-	Game.characters[par.characterID].actions[slot] = data["action"]
+	Game.characters[par.characterID].actions[slot] = current_data["action"]
 	
 	
 	var fullCycleResult = FullCycleResultEvaluator.evaluate(Game)
 	
-	textureRect.texture = data["texture"].texture
+	textureRect.texture = current_data["texture"].texture
 	textureRect.show()
-	tooltip_text = data["tooltip"]
+	tooltip_text = current_data["tooltip"]
 
 
 func _get_index():
 	return get_parent().get_children().find(self)
 
 func empty_slot():
-	print("emptying")
 	current_data = null
 	var slot = _get_index()
 	Game.characters[0].actions[slot] = null
-	textureRect.texture = null
+#	textureRect.texture = null
 	textureRect.hide()
 	tooltip_text = ""
+	pass
