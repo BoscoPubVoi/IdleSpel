17c4edf08756a559618a7c5e7d7eb9a233be6222
Florian van Strien <flori9@live.nl>
2023-10-25 09:14:58 +0200

    CheckMoonPhase

---
+++ GameState/Operations/CheckMoonPhase.gd
@@ -0,0 +1,17 @@
+class_name CheckMoonPhase extends Operation
+
+@export var requiredMoonPhase:Array[int]
+@export var operation:Operation
+
+func construct(requiredMoonPhase_, operation_):
+	requiredMoonPhase = requiredMoonPhase_
+	operation = operation_
+
+func execute(executionState:ExecutionState):
+	if requiredMoonPhase.has(executionState.gameState.placeInCycle):
+		operation.execute(executionState)
+
+func _duplicate():
+	var newOperation = CheckMoonPhase.new()
+	newOperation.construct(requiredMoonPhase, operation._duplicate())
+	return newOperation
