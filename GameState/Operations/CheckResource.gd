063c5d493143176e1db18730f524e0fb061637a1
Florian van Strien <flori9@live.nl>
2023-10-26 13:40:39 +0200

    CheckResource

---
+++ GameState/Operations/CheckResource.gd
@@ -0,0 +1,20 @@
+class_name CheckResource extends Operation
+
+@export var resourceToCheck:String
+@export var resourceMinimum:float
+@export var operation:Operation
+
+func construct(resourceToCheck_, resourceMinimum_, operation_):
+	resourceToCheck = resourceToCheck_
+	resourceMinimum = resourceMinimum_
+	operation = operation_
+
+
+func execute(executionState:ExecutionState):
+	if executionState.gameState.resources[resourceToCheck] >= resourceMinimum:
+		operation.execute(executionState)
+
+func _duplicate():
+	var newOperation = CheckResource.new()
+	newOperation.construct(resourceToCheck, resourceMinimum, operation._duplicate())
+	return newOperation 
--- ResourceDisplay.gd
+++ ResourceDisplay.gd
@@ -2,11 +2,11 @@ extends TextureRect
 
 @export var normalTooltip:String
 @export var associatedResource:String
+@export var flashUntilMouseOver:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -14,4 +14,12 @@ func _process(delta):
 		tooltip_text = normalTooltip + " (" + str(ceil(Game.globalProductionBonuses[associatedResource] * 100)) + "% bonus production)"
 	else:
 		tooltip_text = normalTooltip
-	pass
+
+	if flashUntilMouseOver:
+		self.modulate = Color.from_string("#f8ff00", Color.BLACK) if Engine.get_frames_drawn() % 30 < 15 else Color.WHITE
+	else:
+		self.modulate = Color.WHITE
+
+
+func _on_mouse_entered():
+	flashUntilMouseOver = false
