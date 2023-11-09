9e5c8dc8a880b2f859ea1209c1fc0ccba9c81b75
Florian van Strien <flori9@live.nl>
2023-11-09 14:55:16 +0100

    Some of it works

--- GameState/FullCycleResultEvaluator.gd
+++ GameState/FullCycleResultEvaluator.gd
@@ -2,6 +2,15 @@ class_name FullCycleResultEvaluator
 static func evaluate(gameState:GameState):
 	# Make a copy of the game state, then work on that
 	var copiedGameState = gameState._duplicate()
+	var productionPerCharacter = []
+	
+	for char in copiedGameState.characters:
+		productionPerCharacter.push_back({})
+	
+	# Pretend we have so many resources, you won't believe it
+	for res in copiedGameState.resources:
+		copiedGameState.resourceCaps[res] = 100000000
+		copiedGameState.resources[res] = 10000000
 	
 	# Pretend cycle start
 	copiedGameState.placeInCycle = 0
@@ -30,12 +39,16 @@ static func evaluate(gameState:GameState):
 			)
 			character.actions[placeInCycle].executeEarly(currentExecutionState)
 
-		for character in copiedGameState.characters:
+		for i in len(copiedGameState.characters):
+			var character = copiedGameState.characters[i]
+			
 			if character.skipAllActionsThisCycle:
 				continue
 			
 			if placeInCycle >= len(character.actions) || character.actions[placeInCycle] == null:
 				continue
+				
+			var resourcesPre = copiedGameState.resources.duplicate()
 
 			var currentExecutionState = ExecutionState.new(
 				copiedGameState,
@@ -45,7 +58,15 @@ static func evaluate(gameState:GameState):
 				null
 			)
 			character.actions[placeInCycle].execute(currentExecutionState)
-
+			
+			for res in copiedGameState.resources:
+				if copiedGameState.resources[res] != resourcesPre[res]:
+					var addProd = copiedGameState.resources[res] - resourcesPre[res]
+					if productionPerCharacter.has(res):
+						productionPerCharacter[i][res] += addProd
+					else:
+						productionPerCharacter[i][res] = addProd
+		
 		for character in copiedGameState.characters:
 			if character.skipAllActionsThisCycle:
 				continue
@@ -62,4 +83,6 @@ static func evaluate(gameState:GameState):
 			)
 			character.actions[placeInCycle].executeLate(currentExecutionState)
 
-	return copiedGameState
+	return productionPerCharacter
+	
+	# return copiedGameState
