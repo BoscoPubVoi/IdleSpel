45cf115fa5dff40d542d419ebcd633920bb0a973
Florian van Strien <flori9@live.nl>
2023-12-15 21:20:30 +0100

    hdpi scaling

--- ResourceDisplay.gd
+++ ResourceDisplay.gd
@@ -4,12 +4,29 @@ extends TextureRect
 @export var associatedResource:String
 @export var flashUntilMouseOver:bool = false
 
+var storeddpi = 96
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rescale()
+	
+func rescale():
+	var dpi = DisplayServer.screen_get_dpi()
+	var scalefactor = clamp(dpi / 96.0, 1, 5)
+	var sz = DisplayServer.screen_get_size()
+	if sz.x / scalefactor < 1150:
+		scalefactor = max(sz.x / 1150, 1)
+	if sz.y / scalefactor < 650:
+		scalefactor = max(sz.y / 650, 1)
+	get_tree().root.content_scale_factor = scalefactor
+	storeddpi = dpi
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	if DisplayServer.screen_get_dpi() != storeddpi:
+		rescale()
+	
 	if Game.globalProductionBonuses.has(associatedResource) && Game.globalProductionBonuses[associatedResource] > 0:
 		tooltip_text = normalTooltip + " (" + str(ceil(Game.globalProductionBonuses[associatedResource] * 100)) + "% bonus production)"
 	else:
